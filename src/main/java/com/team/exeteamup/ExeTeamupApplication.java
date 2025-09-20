c6c137ac2316aeb684503240523a67eec744487c
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2025-09-20 09:46:40 +0700

    first commit

---
+++ src/main/java/com/team/exeteamup/ExeTeamupApplication.java
@@ -0,0 +1,13 @@
+package com.team.exeteamup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeTeamupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExeTeamupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/exeteamup/ExeTeamupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.exeteamup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeTeamupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
