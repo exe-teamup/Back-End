72ab09b0581abc276dbe01cc99245cfc1c2961ac
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2025-09-20 10:08:02 +0700

    [FIX] [CONFIG] using env for build project

--- src/test/java/com/team/exeteamup/ExeTeamupApplicationTests.java
+++ src/test/java/com/team/exeteamup/ExeTeamupApplicationTests.java
@@ -1,13 +1,14 @@
 package com.team.exeteamup;
 
 import com.team.exeteamup.config.DotEnvConfig;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExeTeamupApplicationTests {
-    @Test
-    void loadEnvForTest() {
+    @BeforeAll
+    static void loadEnvForTest() {
         DotEnvConfig.loadEnv();
     }
 
