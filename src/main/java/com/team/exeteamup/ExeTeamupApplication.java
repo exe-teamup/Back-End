b769e2a915491246339628a6e44375684bce9124
Hưng Trương <hungtqse182075@fpt.edu.vn>
2025-10-08 11:17:50 +0700

    enable .env

--- src/main/java/com/team/exeteamup/ExeTeamupApplication.java
+++ src/main/java/com/team/exeteamup/ExeTeamupApplication.java
@@ -1,6 +1,5 @@
 package com.team.exeteamup;
 
-import com.team.exeteamup.config.DotEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExeTeamupApplication {
 
     public static void main(String[] args) {
-        DotEnvConfig.loadEnv();
         SpringApplication.run(ExeTeamupApplication.class, args);
     }
 
--- src/test/java/com/team/exeteamup/ExeTeamupApplicationTests.java
+++ src/test/java/com/team/exeteamup/ExeTeamupApplicationTests.java
@@ -1,16 +1,15 @@
 package com.team.exeteamup;
 
-import com.team.exeteamup.config.DotEnvConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExeTeamupApplicationTests {
-    @BeforeAll
-    static void loadEnvForTest() {
-        DotEnvConfig.loadEnv();
-    }
+//    @BeforeAll
+//    static void loadEnvForTest() {
+//        DotEnvConfig.loadEnv();
+//    }
 
     @Test
     void contextLoads() {
