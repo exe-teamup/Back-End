04abc870848ea1c1f0952b963e44b9e31326f22c
Hưng Trương <hungtqse182075@fpt.edu.vn>
2025-11-13 15:23:35 +0700

    [FEAT] get workload

--- src/main/java/com/team/exeteamup/service/inter/DashBoardService.java
+++ src/main/java/com/team/exeteamup/service/inter/DashBoardService.java
@@ -1,7 +1,9 @@
 package com.team.exeteamup.service.inter;
 
 import com.team.exeteamup.dto.response.DashboardStatsResponse;
+import com.team.exeteamup.dto.response.LecturerWorkloadDashboardResponse;
 
 public interface DashBoardService {
     DashboardStatsResponse getDashboardStats();
+    LecturerWorkloadDashboardResponse getLecturerWorkloadDashboard();
 }
